a5c965de0ef6e91ac70163d14bff4fcb153ca109
Hugues Trottier <Hugues_trottier@hotmail.com>
2018-10-02 11:05:11 -0400

    added most? of the necessary states and event in the fsm

---
+++ Doug/StateMachines/BotStateMachine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doug.StateMachines
+{
+    public class BotStateMachine
+    {
+        private enum State
+        {
+            Idle,
+            CoffeeBreakBuilding,
+            CoffeeRemind,
+            CoffeePostponed,
+            CoffeeBreak
+        }
+
+        public enum Event
+        {
+            CoffeeEmoji,
+            CoffeeResolve,
+            CoffeeCancel,
+            CoffeePostpone,
+            CoffeeRemindTimeout,
+            CoffeeBreakEnd
+        }
+    }
+}
